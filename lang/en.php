c2d8db23342357acd2e24342abb24ee5cbc31e90
Villanueva-Flavio <vilanuevaflavioT@gmail.com>
2022-07-12 16:45:21 -0300

    Initial commit

---
+++ lang/en.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of Linfo (c) 2010 Joseph Gillotti.
+ * 
+ * Linfo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Linfo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Linfo. If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
+
+defined('IN_LINFO') or exit;
+
+/*
+ * English translation
+ */
+
+$lang = array(
+	'header' => 'Panel de control',
+	'core' => 'Sistema',
+	'os' => 'Sistema Operativo',
+	'kernel' => 'Kernel',
+	'accessed_ip' => 'Accessed IP',
+	'uptime' => 'Uptime',
+	'hostname' => 'Hostname',
+	'cpus' => 'CPUs',
+	'phpversion' => 'PHP version',
+	'webservice' =>'HTTP server', 
+	'load' => 'Load',
+	'processes' => 'Procesos',
+	'threads' => 'Hilos',
+	'total' => 'Total',
+	'memory' => 'RAM',
+	'type' => 'Tipo',
+	'free' => 'Libre',
+	'used' => 'Usado',
+	'size' => 'Tamaño',
+	'physical' => 'Physical',
+	'swap' => 'Swap',
+	'network_devices' => 'Network Devices',
+	'device_name' => 'Device Name',
+	'amount_sent' => 'Amount Sent',
+	'amount_received' => 'Amount Received',
+	'state' => 'Estado',
+	'temps_voltages' => 'Temperatures / Voltages',
+	'path' => 'Path',
+	'device' => 'Device',
+	'value' => 'Value',
+	'hardware' => 'Hardware',
+	'vendor' => 'Vendor',
+	'drives' => 'Drives',
+	'name' => 'Name',
+	'reads' => 'Reads',
+	'writes' => 'Writes',
+	'filesystem_mounts' => 'Filesystem Mounts',
+	'mount_point' => 'Mount Point',
+	'filesystem' => 'Filesystem',
+	'percent_used' => 'Porcentaje en uso',
+	'raid_arrays' => 'RAID Arrays',
+	'level' => 'Level',
+	'status' => 'Status',
+	'devices' => 'Devices',
+	'label' => 'Label',
+	'active' => 'Active',
+	'batteries' => 'Batteries',
+	'charge' => 'Charge',
+	'unknown' => 'Unknown',
+	'footer_app' => ' ',
+	'none_found' => 'None Found',
+	'sound_cards' => 'Sound Cards',
+	'number' => 'Number',
+	'card' => 'Card',
+	'message' => 'Message',
+	'from_where' => 'Source',
+	'mount_options' => 'Mount Options',
+	'error_head' => 'Data Gathering Errors',
+	'timer' => 'Timer',
+	'area' => 'Area',
+	'time_taken' => 'Time taken to fetch',
+	'days' => 'dias',
+	'years' => 'años',
+	'hours' => 'horas',
+	'minutes' => 'minutos',
+	'seconds' => 'segundos',
+	'pid' => 'PID',
+	'service' => 'Servicio',
+	'services' => 'Servicios',
+	'memory_usage' => 'Uso de memoria',
+	'distro' => 'Distribución',
+	'cpu_arch' => 'Arquitectura',
+	'model' => 'Placa madre',
+	'numLoggedIn' => 'Usuarios activos',
+	'virtualization' => 'Virtualization',
+	'guest' => 'Guest',
+	'host' => 'Host',
+	'cpu_usage' => 'Uso promedio de CPU',
+	'port_speed' => 'Port Speed'
+);
